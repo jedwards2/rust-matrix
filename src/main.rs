40acbfe1b76bf9f90de70cf8a32e164f1f87bc54
Jack <jack.morgan.edwards@gmail.com>
2022-12-25 12:22:04 -0500

    display works for other m and n's

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,34 @@ struct Matrix {
 }
 
 fn main() {
-    let matrix = Matrix {
+    let mut matrix = Matrix {
         array: [0, 1, 2, 3, 4, 5, 6, 7, 8],
         m: 3,
         n: 3,
     };
+
+    display_matrix(&matrix);
+    transpose_matrix(&mut matrix);
     display_matrix(&matrix);
 }
 
 fn display_matrix(matrix: &Matrix) {
+    let short_row: i32;
+    if matrix.m < matrix.n {
+        short_row = matrix.m;
+    } else {
+        short_row = matrix.n
+    }
+
     for i in 0..(matrix.m) {
         for q in 0..(matrix.n) {
-            print!("{}", matrix.array[((i * 3) + q) as usize])
+            print!("{}", matrix.array[((i * short_row) + q) as usize])
         }
         println!();
     }
+}
+
+fn transpose_matrix(matrix: &mut Matrix) -> &mut Matrix {
+    //implement
+    return matrix;
 }
