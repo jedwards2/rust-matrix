ccc7b5fc11fbad741fcd2b690ff5f6752ebbc6d1
Jack <jack.morgan.edwards@gmail.com>
2022-12-25 12:06:48 -0500

    print functino

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-struct Point {
-    x: i32,
-    y: i32,
-}
-
 struct Matrix {
-    array: [i32],
+    array: [i32; 9],
     m: i32,
     n: i32,
 }
 
 fn main() {
-    let point = Point { x: 4, y: 5 };
-    let matrix = println!("x: {} y:{}", point.x, point.y);
+    let matrix = Matrix {
+        array: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        m: 3,
+        n: 3,
+    };
+    display_matrix(&matrix);
+}
+
+fn display_matrix(matrix: &Matrix) {
+    for i in 0..(matrix.m) {
+        for q in 0..(matrix.n) {
+            print!("{}", matrix.array[((i * 3) + q) as usize])
+        }
+        println!();
+    }
 }
