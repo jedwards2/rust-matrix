c20c6e7f8ed5f80a508802ddfc5601ce1a693f9e
Jack <jack.morgan.edwards@gmail.com>
2022-12-25 11:56:19 -0500

    initial

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct Matrix {
+    array: [i32],
+    m: i32,
+    n: i32,
+}
+
+fn main() {
+    let point = Point { x: 4, y: 5 };
+    let matrix = println!("x: {} y:{}", point.x, point.y);
+}
